7774886e524b3cb50ac6819d43a27431912674b8
SeongOh Park <qkrtjddh1212@naver.com>
2024-05-11 11:15:06 +0900

    fix: 빌드 오류 해결

--- lib/gtag.tsx
+++ lib/gtag.tsx
@@ -1,32 +1,32 @@
-// import metadata from 'data/metadata';
+import metadata from 'data/metadata';
 
-// export const GA_TRACKING_ID = metadata.analytics.google;
+export const GA_TRACKING_ID = 'metadata.analytics.google';
 
-// declare global {
-//   interface Window {
-//     gtag: (param1: string, param2: string, param3: object) => void;
-//   }
-// }
+declare global {
+  interface Window {
+    gtag: (param1: string, param2: string, param3: object) => void;
+  }
+}
 
-// // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
-// export const pageview = (url: string) => {
-//   window.gtag('config', GA_TRACKING_ID, {
-//     page_path: url,
-//   });
-// };
+// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
+export const pageview = (url: string) => {
+  window.gtag('config', GA_TRACKING_ID, {
+    page_path: url,
+  });
+};
 
-// type GTagEvent = {
-//   action: string;
-//   category: string;
-//   label: string;
-//   value: number;
-// };
+type GTagEvent = {
+  action: string;
+  category: string;
+  label: string;
+  value: number;
+};
 
-// // https://developers.google.com/analytics/devguides/collection/gtagjs/events
-// export const event = ({ action, category, label, value }: GTagEvent) => {
-//   window.gtag('event', action, {
-//     event_category: category,
-//     event_label: label,
-//     value: value,
-//   });
-// };
+// https://developers.google.com/analytics/devguides/collection/gtagjs/events
+export const event = ({ action, category, label, value }: GTagEvent) => {
+  window.gtag('event', action, {
+    event_category: category,
+    event_label: label,
+    value: value,
+  });
+};
