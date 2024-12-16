a04fe3f754c3dbe2bf69476af737c382c52237be
SeongOh Park <qkrtjddh1212@naver.com>
2024-12-16 13:59:27 +0900

    feat(giscus): category 및 lang 추가

--- src/app/notes/_ui/giscus-comment.tsx
+++ src/app/notes/_ui/giscus-comment.tsx
@@ -11,8 +11,12 @@ const GiscusComment = () => {
       repo="Raoun4136/Raoun4136-dev"
       repoId={'R_kgDOIVQq2Q'}
       mapping={'pathname'}
+      lang={'ko'}
       reactionsEnabled="1"
-      emitMetadata="1"
+      strict="1"
+      emitMetadata="0"
+      category="Comments"
+      categoryId="DIC_kwDOIVQq2c4ClQN4"
       loading="lazy"
       theme={theme.resolvedTheme}
     />
